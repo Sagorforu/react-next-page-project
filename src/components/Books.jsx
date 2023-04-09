7c70d4b018a0c530d16aa24201e429b7f7a3a775
sagorforu <freelancersagor18@gmail.com>
2023-04-09 15:37:05 +0600

    loader added

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigation } from 'react-router-dom';
 import Book from './Book';
+import LoaderSpinner from './LoaderSpinner';
 
 const Books = () => {
-    const {books} = useLoaderData();
+    const navigation = useNavigation()
+    console.log(navigation.state)
+    if (navigation.state === 'loading') {
+        return <LoaderSpinner></LoaderSpinner>
+    }
+    const { books } = useLoaderData();
     console.log(books)
     return (
         <div className='my-container'>
             <div className='grid gap-6 mb-8 lg:grid-cols-4 sm:grid-cols-2'>
                 {
                     books.map(book => <Book
-                    key={book.isbn13}
-                    book={book}
+                        key={book.isbn13}
+                        book={book}
                     ></Book>)
                 }
             </div>
